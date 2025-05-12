1b972e1616f1fcfc495bbf5f8826f784cf62ed33
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-05-12 15:49:21 +0200

    .

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,9 +9,9 @@ import {
 import Fade from "embla-carousel-fade";
 import Autoplay from "embla-carousel-autoplay";
 
-import Hero1 from "../public/hero1.jpg";
-import Hero2 from "../public/hero2.jpg";
-import Hero3 from "../public/hero3.jpg";
+import Hero1 from "../public/22892967_6689882.jpg";
+import Hero2 from "../public/2149049660.jpg";
+import Hero3 from "../public/3853.jpg";
 
 import Image from "next/image";
 import { ArrowRightIcon } from "lucide-react";
@@ -35,7 +35,7 @@ const Hero = () => {
           Fade(),
         ]}
       >
-        <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-[1]" />
+        {/* <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-[1]" /> */}
         <CarouselContent>
           <CarouselItem>
             <Image
